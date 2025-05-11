a0b00e038037742f75a5cc2a1fd1984a087b7e7e
jaehoonchoi <eeoos8840@gmail.com>
2025-05-11 17:50:06 +0900

    solve 0101

---
+++ src/algorithm/inflearn/introduction/I0101.java
@@ -0,0 +1,22 @@
+package algorithm.inflearn.introduction;
+
+import java.util.*;
+
+public class I0101 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String i1 = sc.next().toLowerCase();
+		String i2 = sc.next().toLowerCase();
+		
+		int result = 0;
+		for(int i = 0; i<i1.length(); i++) {
+			String tmp = String.valueOf(i1.charAt(i));
+			if(i2.equals(tmp)) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
