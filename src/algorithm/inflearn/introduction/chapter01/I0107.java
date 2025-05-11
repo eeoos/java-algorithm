86d15e85bca8493d0cba40fd3612f0ed40fb1b41
jaehoonchoi <eeoos8840@gmail.com>
2025-05-11 18:46:20 +0900

    solve 0107

--- src/algorithm/inflearn/introduction/chapter01/I0107.java
+++ src/algorithm/inflearn/introduction/chapter01/I0107.java
@@ -2,21 +2,21 @@ package algorithm.inflearn.introduction.chapter01;
 
 import java.util.*;
 
-public class I0106 {
+public class I0107 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
+		
 		StringBuilder sb = new StringBuilder();
 		
-		for(int i = 0; i<input.length(); i++) {
-			String tmp = sb.toString();
-			char c = input.charAt(i);
-			if(!tmp.contains(String.valueOf(c))) {
-				sb.append(c);
-			}
+		sb.append(input);
+		
+		if(input.equalsIgnoreCase(sb.reverse().toString())) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
 		}
-		System.out.println(sb.toString());
 	}
 
 }
