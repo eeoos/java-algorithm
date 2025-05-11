d190b5df862c1f2b1c42d25311713ffa9ad5a27d
jaehoonchoi <eeoos8840@gmail.com>
2025-05-11 19:04:36 +0900

    solve inflearn introduction 0108

--- src/algorithm/inflearn/introduction/chapter01/I0108.java
+++ src/algorithm/inflearn/introduction/chapter01/I0108.java
@@ -2,22 +2,27 @@ package algorithm.inflearn.introduction.chapter01;
 
 import java.util.*;
 
-public class I0107 {
+public class I0108 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
-		
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append(input);
+		String input = sc.nextLine();
 		
-		if(input.equalsIgnoreCase(sb.reverse().toString())) {
+		for(int i = 0 ; i<input.length(); i++) {
+			char c = input.charAt(i);
+			if(Character.isAlphabetic(c)) {
+				sb.append(c);
+			}
+		}
+		
+		if(sb.toString().equalsIgnoreCase(sb.reverse().toString())) {
 			System.out.println("YES");
 		}else {
 			System.out.println("NO");
 		}
+
 	}
 
 }
-	
