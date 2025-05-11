2d667d6304ea86d889e6e59f03d3c9a4ad20e5bd
jaehoonchoi <eeoos8840@gmail.com>
2025-05-11 17:52:39 +0900

    solve 0102

---
+++ src/algorithm/inflearn/introduction/I0102.java
@@ -0,0 +1,18 @@
+package algorithm.inflearn.introduction;
+
+import java.util.*;
+
+public class I0102 {
+	 public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			char[] input= sc.next().toCharArray();
+			StringBuilder sb = new StringBuilder();
+			for(char i : input) {
+				char tmp = Character.isLowerCase(i) ? Character.toUpperCase(i) : Character.toLowerCase(i);
+				sb.append(tmp);
+			}
+			System.out.println(sb.toString());
+
+			
+	 	}
+}
