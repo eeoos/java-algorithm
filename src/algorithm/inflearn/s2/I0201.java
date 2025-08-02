69ad4ad3893852e771c1c512cad79df38eed5bca
jaehoonchoi <eeoos8840@gmail.com>
2025-08-02 21:51:51 +0900

    fix: I0201 오답 수정

--- src/algorithm/inflearn/s2/I0201.java
+++ src/algorithm/inflearn/s2/I0201.java
@@ -4,14 +4,21 @@ import java.util.*;
 public class I0201 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		List<String> list = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
 			
 		int T = sc.nextInt();
 		sc.nextLine();
-		for(String s :sc.nextLine().split(" ")) {
-			int tmp = Integer.parseInt(s);
-			if(T <= tmp) list.add(s); 
+		int tmp = sc.nextInt();
+		list.add(tmp);
+		for(int i = 0; i<T-1; i++) {
+			int a = sc.nextInt();
+			if (tmp < a) list.add(a);
+			tmp = a;
 		}
-		System.out.println(String.join(" ", list));
+		
+		
+		for(Integer r : list) {
+			System.out.print(r + " ");
+		}
 	}
 }
