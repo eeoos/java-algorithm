00a83a1b9431975ee9d27d257aed9e93de661712
jaehoonchoi <eeoos8840@gmail.com>
2026-02-04 15:46:50 +0900

    feat: solve I0302

--- src/algorithm/inflearn/s3/I0302.java
+++ src/algorithm/inflearn/s3/I0302.java
@@ -1,34 +1,48 @@
 package algorithm.inflearn.s3;
 
 import java.util.*;
+import java.io.*;
+  
 public class I0302 {
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    
+    
+    int n = Integer.parseInt(br.readLine());
+    int[] a = StringToArr(n, br.readLine());
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		List<Integer> l = new ArrayList<>();
-		
-		int a = sc.nextInt();
-		int[] aArr = new int[a];
-		for(int i = 0; i<a; i++) {
-			aArr[i] = sc.nextInt();
-		}
-		
-		int b = sc.nextInt();
-		
-		for(int i = 0; i<b; i++) {
-			int tmp = sc.nextInt();
-			
-			for(int k =0; k<a; k++) {
-					if(aArr[k] == tmp) l.add(aArr[k]);
-			}
-		}
-		
-		Collections.sort(l);
-		
-		for(int i = 0; i<l.size(); i++) {
-			System.out.print(l.get(i) + " ");
-		}
-	}
-
+	int m = Integer.parseInt(br.readLine());
+    int[] b = StringToArr(m, br.readLine());
+	
+    int p1= 0, p2=0;
+    int i = 0;
+    while(p1 < n && p2 < m){
+      
+      
+      if(a[p1]<b[p2]) p1++;
+      else if(a[p1]>b[p2]) p2++;
+      else{
+        if(i>0) sb.append(" "); 
+        sb.append(a[p1]);
+        p1++;
+        p2++;
+        i++;
+      }
+      
+    }
+    
+    System.out.println(sb);
+  }
+  
+  static int[] StringToArr(int l, String input){
+    StringTokenizer st = new StringTokenizer(input);
+    int[] arr = new int[l];
+    for(int i = 0; i<l; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr);
+    return arr;
+  }
 }
