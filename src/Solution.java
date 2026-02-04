2c8ec44da4c6bb8a103a50161aa62e2f2d130acd
jaehoonchoi <eeoos8840@gmail.com>
2026-02-04 11:05:42 +0900

    feat: solve I0501

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,26 @@
 import java.util.*;
 import java.io.*;
-  
+
 public class Solution {
   public static void main(String[] args) throws IOException {
     
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 	StringTokenizer st = new StringTokenizer(br.readLine());
+    char[] arr = br.readLine().trim().toCharArray();
     
-    int N = Integer.parseInt(st.nextToken());
-    int K = Integer.parseInt(st.nextToken());
-    int[] arr = new int[N];
-   	int cnt = 0;
-	List<Integer> iarr = new ArrayList<>();
+    Stack<Character> s = new Stack<>();
     
-    st = new StringTokenizer(br.readLine());
-    for(int i = 0; i<N; i++){
- 	  arr[i] = Integer.parseInt(st.nextToken());
-    }
     
-    for(int i = 0; i<=N-K; i++){
-      int sum = 0;
-      for(int j = 0; j<3; j++){
-        sum += arr[i+j];
-      }
-      iarr.add(sum);
-    }
-    Collections.sort(iarr, Collections.reverseOrder());
-    System.out.println(iarr);
-    int tmp = 0;
-    for(int i = 0; i<iarr.size(); i++){
-      if(iarr.get(i) != tmp){
-        tmp = iarr.get(i);
-        cnt++;
-        if(cnt == K){
-          System.out.println(tmp);
-          return;
-        }
+   	for(char c : arr){
+      if('(' == c){
+        s.push(c);
+      } else if(')' == c){
+        s.pop();
       }
     }
-    System.out.println(-1);
+    
+    String result = "";
+    result = s.size() != 0 ? "NO" : "YES";
+    System.out.println(result);
+    
   }
 }
--- src/algorithm/inflearn/s4/I0405.java
+++ src/algorithm/inflearn/s4/I0405.java
@@ -1,5 +1,46 @@
 package algorithm.inflearn.s4;
 
+import java.util.*;
+import java.io.*;
+  
 public class I0405 {
-
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 	StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    int[] arr = new int[N];
+   	int cnt = 0;
+	List<Integer> iarr = new ArrayList<>();
+    
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i<N; i++){
+ 	  arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    for(int i = 0; i<N-2; i++){
+      for(int j = i+1; j<N-1; j++){
+        for(int k = j+1; k<N; k++){
+          int t = arr[i] + arr[j] + arr[k];
+          iarr.add(t);
+        }
+      }
+    }
+    
+    Collections.sort(iarr, Collections.reverseOrder());
+    int tmp = 0;
+    for(int i = 0; i<iarr.size(); i++){
+      if(iarr.get(i) != tmp){
+        tmp = iarr.get(i);
+        cnt++;
+        if(cnt == K){
+          System.out.println(tmp);
+          return;
+        }
+      }
+    }
+    System.out.println(-1);
+  }
 }
