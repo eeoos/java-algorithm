faa0d40288d55f419697be0eca086e911ed7b5d5
jaehoonchoi <eeoos8840@gmail.com>
2026-02-05 16:59:47 +0900

    feat: solve I0402 with Map

---
+++ src/algorithm/inflearn/s4/I0402_2.java
@@ -0,0 +1,34 @@
+package algorithm.inflearn.s4;
+
+import java.util.*;
+import java.io.*;
+  
+public class I0402_2 {
+  public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    char[] a = br.readLine().trim().toCharArray();
+    char[] b = br.readLine().trim().toCharArray();
+    
+    Map<Character, Integer> m = new HashMap<>();
+   	for(char c : a){
+      m.put(c, m.getOrDefault(c, 0)+1);
+    }
+    
+    for(char c : b){
+      if(m.get(c) == null){
+        System.out.println("NO");
+        return;
+      }
+      m.put(c, m.get(c)-1);
+    }
+    
+    for(Map.Entry<Character, Integer> c : m.entrySet()){
+		if(c.getValue() != 0){
+          System.out.println("NO");
+          return;
+        }
+    }
+    System.out.println("YES");
+  }
+}
