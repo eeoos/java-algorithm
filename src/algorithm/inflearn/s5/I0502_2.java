78c1484550ea2b7150bc383fba89c54b38fbfaca
jaehoonchoi <eeoos8840@gmail.com>
2026-02-06 15:29:06 +0900

    feat: solve I0503

--- src/algorithm/inflearn/s5/I0502_2.java
+++ src/algorithm/inflearn/s5/I0502_2.java
@@ -1,5 +1,26 @@
 package algorithm.inflearn.s5;
 
+import java.util.*;
+import java.io.*;
+  
 public class I0502_2 {
-
+  public static void main(String[] args) throws IOException {
+	
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    
+    char[] arr = br.readLine().toCharArray();
+    Deque<Character> dq = new ArrayDeque<>();
+    for(char c : arr){
+      if(c == ')') while(dq.pop() != '(');
+      else dq.push(c);
+    }
+    
+    int size = dq.size();
+    for(int i =0; i<size; i++) {
+    	sb.append(dq.pollLast());
+    }
+    
+    System.out.println(sb);
+  }
 }
--- src/algorithm/inflearn/s5/I0503_2.java
+++ src/algorithm/inflearn/s5/I0503_2.java
@@ -1,7 +1,9 @@
+package algorithm.inflearn.s5;
+
 import java.util.*;
 import java.io.*;
   
-public class Main {
+public class I0503_2 {
   public static void main(String[] args) throws IOException {
     
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,14 +22,14 @@ public class Main {
     st = new StringTokenizer(br.readLine());
     Deque<Integer> dq = new ArrayDeque<>();
     while(m-- >0){
-      int tmp = Integer.parseInt(st.nextToken());
+      int tmp = Integer.parseInt(st.nextToken()) -1;
       for(int i = 0; i<n; i++){
         if(arr[i][tmp] != 0){
-          arr[i][tmp] = 0;
           int e = arr[i][tmp];
+          arr[i][tmp] = 0;
           if(!dq.isEmpty() && dq.peek() == e){
-            dq.pop();
             cnt+=2;
+            dq.pop();
           }else dq.push(e);
           break;
         }
