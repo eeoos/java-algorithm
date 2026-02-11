90dc14c570596b736efa2bee458963cb66cd9332
jaehoonchoi <eeoos8840@gmail.com>
2026-02-12 02:08:20 +0900

    feaet: solve I0604

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,47 @@ import java.io.*;
   
 public class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
-	StringBuilder sb = new StringBuilder();
+    
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    
+    int T = Integer.parseInt(st.nextToken());
+    int n = Integer.parseInt(st.nextToken());
     
     int[] arr = new int[T];
-    StringTokenizer st= new StringTokenizer(br.readLine());
-    for(int i = 0; i<T; i++){
-      arr[i] = Integer.parseInt(st.nextToken());
+    
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i<n; i++){
+      int in = Integer.parseInt(st.nextToken());
+      int pos = -1;
+      for(int j = 0; j<T; j++)
+        if(arr[j] == in)  pos= j;
+      
+      int j = T-1;
+      if(pos==-1){
+        while(j>0){
+          arr[j] = arr[j-1];
+          j--;
+        }
+        arr[j] = in;
+      }else{
+        j = pos;
+        while(j>0){
+          arr[j] = arr[j-1];
+          j--;
+        }
+        arr[j] = in;
+      }
+      
     }
     
     for(int i = 0; i<T; i++){
-      int min = arr[i];
-      for(int j = i+1; j<T; j++){
-        int minPos = 0;
-        if(min > arr[j]){
-		  min = arr[j];
-          minPos = j;
-        }
-        int tmp = arr[minPos];
-        arr[minPos] = arr[i];
-        arr[i] = tmp;
-      }
       if(i>0) sb.append(" ");
       sb.append(arr[i]);
     }
     
     System.out.println(sb);
-  }
+    
+  }   
 }
--- src/algorithm/inflearn/s6/I0603.java
+++ src/algorithm/inflearn/s6/I0603.java
@@ -1,5 +1,37 @@
 package algorithm.inflearn.s6;
 
+import java.util.*;
+import java.io.*;
+  
 public class I0603 {
-
+  public static void main(String[] args) throws IOException {
+	
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    
+    int T = Integer.parseInt(br.readLine());
+    int[] arr= new int[T];
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i = 0; i<T; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    for(int i =1; i<T; i++){
+      int tmp = arr[i];
+      int j = i;
+      while((j>0) && (arr[j-1] > tmp)){
+        arr[j] = arr[j-1];
+        j--;
+      }
+      arr[j] = tmp;
+    }
+    
+    for(int i = 0; i<T; i++){
+      if(i>0) sb.append(" ");
+      sb.append(arr[i]);
+    }
+    
+    System.out.println(sb);
+  }
 }
