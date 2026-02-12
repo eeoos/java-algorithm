d17e3e6c4cb1e49f903b65631c38afa037f81725
jaehoonchoi <eeoos8840@gmail.com>
2026-02-12 15:10:56 +0900

    feat: solve I0606 using compareTo method

---
+++ src/algorithm/inflearn/s6/I0605_3.java
@@ -0,0 +1,5 @@
+package algorithm.inflearn.s6;
+
+public class I0605_3 {
+
+}
---
+++ src/algorithm/inflearn/s6/I0606_2.java
@@ -0,0 +1,37 @@
+package algorithm.inflearn.s6;
+
+import java.util.*;
+import java.io.*;
+
+class Point implements Comparable<Point> {
+  public int x, y;
+  Point(int x, int y){this.x = x; this.y = y;}
+  
+  @Override
+  public int compareTo(Point o){
+  	if(this.x == o.x) return this.y - o.y;
+    else return this.x - o.x;
+  }
+}
+
+public class I0606_2 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+    Point[] arr = new Point[T];
+    StringTokenizer st;
+    for(int i =0; i<T; i++){
+      st = new StringTokenizer(br.readLine());
+      arr[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+    }
+    
+    Arrays.sort(arr);
+    for(int i = 0; i<T; i++){
+      if(i>0) sb.append("\n");
+      sb.append(arr[i].x).append(" ").append(arr[i].y);
+    }
+    
+    System.out.println(sb);
+  }
+}
