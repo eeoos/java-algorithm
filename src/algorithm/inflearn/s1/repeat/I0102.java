274bcc46330bfe5cd7ac75c220bb8524db617344
jaehoonchoi <eeoos8840@gmail.com>
2026-03-11 23:46:17 +0900

    feat: solve I0102

---
+++ src/algorithm/inflearn/s1/repeat/I0102.java
@@ -0,0 +1,18 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+
+public class I0102 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    char[] arr = br.readLine().toCharArray();
+    
+    for(char c : arr){
+      if(Character.isUpperCase(c)) sb.append(Character.toLowerCase(c));
+      else sb.append(Character.toUpperCase(c));
+    }
+    
+    System.out.println(sb);
+  }
+}
