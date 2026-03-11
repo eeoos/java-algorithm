85247a9bc5dd367a41b2ea8d95dfeb3c99f65655
jaehoonchoi <eeoos8840@gmail.com>
2026-03-12 00:39:18 +0900

    feat: solve I0104

---
+++ src/algorithm/inflearn/s1/repeat/I0104.java
@@ -0,0 +1,26 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+
+public class I0104 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    
+    for(int i = 0; i<n; i++){
+      char[] arr = br.readLine().toCharArray();
+      int lt = 0;
+      int rt = arr.length-1;
+      
+      while(lt<rt){  
+        char tmp = arr[lt];
+        arr[lt] = arr[rt];
+        arr[rt]= tmp;        
+        lt++;
+      	rt--;
+      }
+      
+      System.out.println(String.valueOf(arr));
+    }
+  }
+}
