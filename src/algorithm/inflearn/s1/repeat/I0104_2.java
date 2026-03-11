f236fb97abd94ccfb73e7d6e754dd0ae20d95eb0
jaehoonchoi <eeoos8840@gmail.com>
2026-03-12 00:41:28 +0900

    feat: solve I0104

---
+++ src/algorithm/inflearn/s1/repeat/I0104_2.java
@@ -0,0 +1,16 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+
+public class I0104_2 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    
+    for(int i = 0; i<n; i++){
+      sb.setLength(0);
+      System.out.println(sb.append(br.readLine()).reverse());
+    }
+  }
+}
