20ed0dab8853b4369e546e92d4b2488c8a89d5ab
jaehoonchoi <eeoos8840@gmail.com>
2026-03-11 23:37:30 +0900

    feat: solve I0101

---
+++ src/algorithm/inflearn/s1/repeat/I0101.java
@@ -0,0 +1,18 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+  
+public class I0101 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] s = br.readLine().toUpperCase().toCharArray();
+    
+    int result = 0;
+    char t = br.readLine().toUpperCase().charAt(0);
+    for(char c : s){
+      if(t == c) result++;
+    }
+    
+    System.out.println(result);
+  }
+}
