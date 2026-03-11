65135ed65da0eff53dd3fd1e172276df11d9decb
jaehoonchoi <eeoos8840@gmail.com>
2026-03-11 23:39:54 +0900

    feat: solve I0101 using Character.toUpperCase()

---
+++ src/algorithm/inflearn/s1/repeat/I0101_2.java
@@ -0,0 +1,18 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+
+public class I0101_2 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] s = br.readLine().toUpperCase().toCharArray();
+    
+    int result = 0;
+    char t = Character.toUpperCase(br.readLine().charAt(0));
+    for(char c : s){
+      if(Character.toUpperCase(c) == t) result++;
+    }
+    
+    System.out.println(result);
+  }
+}
