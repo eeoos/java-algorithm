d43e895092a3e0a974510e9a11518f97ecaed8b0
jaehoonchoi <eeoos8840@gmail.com>
2026-03-13 20:43:19 +0900

    feat: solve I0105

---
+++ src/algorithm/inflearn/s1/repeat/I0105_2.java
@@ -0,0 +1,26 @@
+package algorithm.inflearn.s1.repeat;
+
+import java.io.*;
+
+public class I0105_2 {
+  public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] arr = br.readLine().toCharArray();
+    
+    int lt =0, rt = arr.length-1;
+    
+    while(lt<rt){
+      if(!Character.isAlphabetic(arr[lt])) lt++;
+	  else if(!Character.isAlphabetic(arr[rt])) rt--;
+      else {
+        char tmp = arr[lt];
+        arr[lt] = arr[rt];
+        arr[rt] = tmp;
+        rt--;
+        lt++;
+      }
+    }
+    
+    System.out.println(String.valueOf(arr));
+  }
+}
